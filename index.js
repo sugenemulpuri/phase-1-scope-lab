907dd032bf35df066785c84b0fd282d647ca2633
Sugene Mulpuri <sugenemulpuri9@gmail.com>
2024-02-22 11:18:57 -0600

    phase 1 scope lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+
+var customerName = 'bob';
+
+
+
+var upperCaseCustomerName = () => {
+    customerName = customerName.toUpperCase()
+}
+
+var setBestCustomer = () => {
+    return bestCustomer = 'not bob';
+}
+
+var overwriteBestCustomer = () => {
+    bestCustomer = 'maybe bob';
+}
+
+const leastfavoriteCustomer = 'tom';
+
+var changeLeastFavoriteCustomer = () => {
+   leastfavoriteCustomer = 'joe';
+}
